80af8e110535d217d249c92e607aef4ca8d82d7a
DominasPL <dominik-stepuch@wp.pl>
2020-06-27 13:25:28 +0200

    Initial commit

---
+++ src/main/java/com/github/dominaspl/convertapp/ConvertFileAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.dominaspl.convertapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertFileAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConvertFileAppApplication.class, args);
+    }
+
+}
