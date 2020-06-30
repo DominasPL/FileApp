4428fea441f27831066cde230bf0aae59dbf907e
DominasPL <dominik-stepuch@wp.pl>
2020-07-01 01:12:04 +0200

    Refactor

--- src/main/java/com/github/dominaspl/convertapp/domain/error/ConstraintValidationError.java
+++ src/main/java/com/github/dominaspl/convertapp/domain/error/ConstraintValidationError.java
@@ -17,28 +17,8 @@ public class ConstraintValidationError {
         this.status = HttpStatus.BAD_REQUEST.toString();
     }
 
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
     public String getMessage() {
         return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
     }
 
     private String parseTime(LocalDateTime time) {
